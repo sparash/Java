b71081d2de67e597b93e96a7aa8f75d488faddad
Sparash sharma <sparash.27kpr@gmail.com>
2018-08-19 14:18:29 +0000

    hello

---
+++ l4code1.java
@@ -0,0 +1,15 @@
+class l4code1
+{
+    public static void main(String[] args)
+    {
+        int test = 5;
+        if(test<10)
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+}
